cc4a8466b5eb48f6ef7b58a0284993fca3fada96
renmu123 <1101022351@qq.com>
2024-07-23 14:39:22 +0800

    refactor(xml): 优化录播姬 XML 转换为 ASS 的性能 (#82)
    * 只对录播姬的gift的raw进行解析来加快速度
    
    * 移除调试代码
    
    * 优化xmlUnescape函数
    
    * refactor(xml): better code structure
    
    fix #67
    
    ---------
    
    Co-authored-by: muse <fyx@030buy.jp>
    Co-authored-by: Shawn Sun <lengyanyu258@outlook.com>

--- XmlFile.c
+++ XmlFile.c
@@ -116,7 +116,7 @@ int readXml(const char *const ipFile, DANMAKU **head, const char *mode, const fl
     BOOL isDanmaku;
     BOOL hasUserInfo;
     BOOL hasGiftInfo;
-    
+
     while (!feof(ipF))
     {
         type = 0;
@@ -284,62 +284,66 @@ int readXml(const char *const ipFile, DANMAKU **head, const char *mode, const fl
             // BililiveRecorder
             else if (strcmp(key, "raw") == 0)
             {
-                strGetLeftPart(NULL, &labelPtr, '\"', LABEL_LEN);
-                strGetLeftPart(raw, &labelPtr, '\"', LABEL_LEN);
-
-                /* 解析raw部分 */
-                char rawKey[KEY_LEN];
-                char rawValue[VALUE_LEN];
-                char *rawPtr = raw;
-
-                char coinTypeValue[VALUE_LEN] = {0};
-
-                xmlUnescape(raw);
-                strGetLeftPart(NULL, &rawPtr, '{', LABEL_LEN);
-
-                while (*rawPtr != '\0')
+                if(hasGiftInfo == TRUE) 
                 {
-                    strGetLeftPart(rawKey, &rawPtr, ':', KEY_LEN);
-                    strGetLeftPart(rawValue, &rawPtr, ',', VALUE_LEN);
-                    deQuotMarks(rawKey);
-                    deQuotMarks(rawValue);
-                    if (strcmp(rawKey, "gift_name") == 0)
+                    strGetLeftPart(NULL, &labelPtr, '\"', LABEL_LEN);
+                    strGetLeftPart(raw, &labelPtr, '\"', LABEL_LEN);
+
+                    /* 解析raw部分 */
+                    char rawKey[KEY_LEN];
+                    char rawValue[VALUE_LEN];
+                    char *rawPtr = raw;
+
+                    char coinTypeValue[VALUE_LEN] = {0};
+                    xmlUnescape(raw);
+                    strGetLeftPart(NULL, &rawPtr, '{', LABEL_LEN);
+
+                    while (*rawPtr != '\0')
                     {
-                        strSafeCopy(gift.name, rawValue, GIFT_NAME_LEN);
-                    }
-                    else if (strcmp(rawKey, "coin_type") == 0)
-                    {
-                        strSafeCopy(coinTypeValue, rawValue, GIFT_NAME_LEN);
-                    }
-                    else if (strcmp(rawKey, "uid") == 0)
-                    {
-                        if (user.uid == 0) {
-                            user.uid = strtoull(rawValue, NULL, 10);
-                        }
-                    }
-                    else if (strcmp(rawKey, "price") == 0)
-                    {
-                        if (FLOAT_IS_EQUAL(gift.price, -1.00))
+                        strGetLeftPart(rawKey, &rawPtr, ':', KEY_LEN);
+                        strGetLeftPart(rawValue, &rawPtr, ',', VALUE_LEN);
+                        deQuotMarks(rawKey);
+                        deQuotMarks(rawValue);
+                        if (strcmp(rawKey, "gift_name") == 0)
                         {
-                            gift.price = atof(rawValue);
+                            strSafeCopy(gift.name, rawValue, GIFT_NAME_LEN);
+                        }
+                        else if (strcmp(rawKey, "coin_type") == 0)
+                        {
+                            strSafeCopy(coinTypeValue, rawValue, GIFT_NAME_LEN);
+                        }
+                        else if (strcmp(rawKey, "uid") == 0)
+                        {
+                            if (user.uid == 0) {
+                                user.uid = strtoull(rawValue, NULL, 10);
+                            }
+                        }
+                        else if (strcmp(rawKey, "price") == 0)
+                        {
+                            if (FLOAT_IS_EQUAL(gift.price, -1.00))
+                            {
+                                gift.price = atof(rawValue);
+                            }
+                        }
+                        else if (strcmp(rawKey, "combo_stay_time") == 0)
+                        {
+                            if (gift.duration == 0) {
+                                gift.duration = GET_MS_FLT(atof(rawValue));
+                            }
                         }
                     }
-                    else if (strcmp(rawKey, "combo_stay_time") == 0)
+
+                    if (strcmp(coinTypeValue, "silver") == 0)
                     {
-                        if (gift.duration == 0) {
-                            gift.duration = GET_MS_FLT(atof(rawValue));
-                        }
+                        giftPriceUnit = 0.0;
                     }
+                    else if (messageType != MSG_SUPER_CHAT)
+                    {
+                        giftPriceUnit = 1e-3;
+                    }
+                            
                 }
 
-                if (strcmp(coinTypeValue, "silver") == 0)
-                {
-                    giftPriceUnit = 0.0;
-                }
-                else if (messageType != MSG_SUPER_CHAT)
-                {
-                    giftPriceUnit = 1e-3;
-                }
             }
             // blrec
             else if (strcmp(key, "cointype") == 0) {
@@ -728,88 +732,58 @@ int writeXml(char const *const fileName, DANMAKU *danmakuHead, STATUS *const sta
   */
 static char *xmlUnescape(char *const str)
 {
-    /* 非法检查 */
     if (str == NULL)
     {
         return NULL;
     }
 
-    char *leftPtr, *rightPtr, *reWritePtr;
-    leftPtr = str;
-    while (*leftPtr != '\0')
+    char *srcPtr = str; // 源字符串指针
+    char *dstPtr = str; // 目标字符串指针，也用于就地修改字符串
+
+    while (*srcPtr != '\0')
     {
-        if (*leftPtr == '&')
+        if (*srcPtr != '&')
         {
-            if (strstr(leftPtr, "&amp;") == leftPtr)
-            {
-                *leftPtr = '&';
-                reWritePtr = leftPtr + 1;
-                rightPtr = leftPtr + 5;
-                while (*rightPtr != '\0')
-                {
-                    *reWritePtr = *rightPtr;
-                    reWritePtr++;
-                    rightPtr++;
-                }
-                *reWritePtr = '\0';
-            }
-            else if (strstr(leftPtr, "&apos;") == leftPtr)
-            {
-                *leftPtr = '\'';
-                reWritePtr = leftPtr + 1;
-                rightPtr = leftPtr + 6;
-                while (*rightPtr != '\0')
-                {
-                    *reWritePtr = *rightPtr;
-                    reWritePtr++;
-                    rightPtr++;
-                }
-                *reWritePtr = '\0';
-            }
-            else if (strstr(leftPtr, "&gt;") == leftPtr)
-            {
-                *leftPtr = '>';
-                reWritePtr = leftPtr + 1;
-                rightPtr = leftPtr + 4;
-                while (*rightPtr != '\0')
-                {
-                    *reWritePtr = *rightPtr;
-                    reWritePtr++;
-                    rightPtr++;
-                }
-                *reWritePtr = '\0';
-            }
-            else if (strstr(leftPtr, "&lt;") == leftPtr)
-            {
-                *leftPtr = '<';
-                reWritePtr = leftPtr + 1;
-                rightPtr = leftPtr + 4;
-                while (*rightPtr != '\0')
-                {
-                    *reWritePtr = *rightPtr;
-                    reWritePtr++;
-                    rightPtr++;
-                }
-                *reWritePtr = '\0';
-            }
-            else if (strstr(leftPtr, "&quot;") == leftPtr)
-            {
-                *leftPtr = '\"';
-                reWritePtr = leftPtr + 1;
-                rightPtr = leftPtr + 6;
-                while (*rightPtr != '\0')
-                {
-                    *reWritePtr = *rightPtr;
-                    reWritePtr++;
-                    rightPtr++;
-                }
-                *reWritePtr = '\0';
-            }
+            // 如果当前字符不是 '&'，就原样复制
+            *dstPtr++ = *srcPtr++;
+            continue;
         }
-        
-        leftPtr++;
+
+        if (strncmp(srcPtr, "&amp;", 5) == 0)
+        {
+            *dstPtr++ = '&';
+            srcPtr += 5;
+        }
+        else if (strncmp(srcPtr, "&apos;", 6) == 0)
+        {
+            *dstPtr++ = '\'';
+            srcPtr += 6;
+        }
+        else if (strncmp(srcPtr, "&gt;", 4) == 0)
+        {
+            *dstPtr++ = '>';
+            srcPtr += 4;
+        }
+        else if (strncmp(srcPtr, "&lt;", 4) == 0)
+        {
+            *dstPtr++ = '<';
+            srcPtr += 4;
+        }
+        else if (strncmp(srcPtr, "&quot;", 6) == 0)
+        {
+            *dstPtr++ = '\"';
+            srcPtr += 6;
+        }
+        else
+        {
+            // 如果不是已知的转义序列，就原样复制
+            *dstPtr++ = *srcPtr++;
+        }
     }
-    
+
+    // 字符串结束
+    *dstPtr = '\0';
+
     return str;
 }
 
