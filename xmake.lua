e41bbbcd6111bc12d82dde3dde75f9f9d99a9d16
Shawn Sun <lengyanyu258@outlook.com>
2025-09-01 02:46:12 +0800

    ci(build): update workflow

--- xmake.lua
+++ xmake.lua
@@ -7,9 +7,9 @@ add_requires("pcre2")
 target("cli")
     set_basename("DanmakuFactory")
     set_kind("binary")
-    set_languages("c11", {strict = true})
-    set_version("2.0.0")
+    set_languages("c11")
+    set_version("2.0.0", {build = "%Y%m%d%H%M"})
 
-    add_files("src/*.c")
-    add_files("src/**/*.c")
+    add_files("src/*.c", "src/**/*.c")
+
     add_packages("pcre2")
