b312ba9b1c714a9c340e8d16cc398e29a4e4d8b8
sieveLau <sievelau@gmail.com>
2025-09-14 00:38:57 +0800

    Update xmake.lua to consider POSIX
    readlink() needs to define correct macro for proper use under Linux. See https://stackoverflow.com/questions/66862654/why-does-my-compiler-think-my-readlink-is-implicitly-declared-if-i-set-the-sta

--- xmake.lua
+++ xmake.lua
@@ -10,6 +10,7 @@ target("cli")
     set_basename(PROJECT_NAME)
     set_kind("binary")
     set_languages("c11")
+    add_defines("_POSIX_C_SOURCE=200809L")
     set_version("2.0.0", {build = "%Y%m%d%H%M"})
 
     add_files("src/*.c", "src/**/*.c")
